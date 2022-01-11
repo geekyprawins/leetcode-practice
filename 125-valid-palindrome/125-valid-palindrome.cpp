d213abd0846de77d2bf54e606587e42e47521734
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2022-01-11 22:31:36 +0530

    Time: 4 ms (92.23%), Space: 7.4 MB (74.13%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0, j = s.length()-1;
+        while(i<j){
+            if(!isalnum(s[i]))
+                i++;
+            else if(!isalnum(s[j]))
+                j--;
+            else {
+                if( tolower(s[i++]) != tolower(s[j--]) ) {
+                    return false;
+                }
+               
+            }
+        }
+        return true;
+    }
+};
