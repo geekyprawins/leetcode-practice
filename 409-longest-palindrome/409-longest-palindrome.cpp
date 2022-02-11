5e686ce22d7b145b42591234103779b2874e791d
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2022-02-11 22:19:03 +0530

    Time: 4 ms (69.17%), Space: 6.6 MB (49.59%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+          unordered_map<char,int> umap;
+        
+        for( char c : s ){
+            umap[c]++;
+        }
+        int rem = 0;
+        int maxl = 0;
+        for( auto itr = umap.begin(); itr != umap.end(); itr++ ){
+            if( rem == 0 && itr->second%2 == 1 ){
+                rem++;
+            }
+            maxl += itr->second - itr->second%2;
+        }
+        
+        
+        return maxl+rem ;
+    }
+};
