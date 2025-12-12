1f53173b635014a36dfaa057437b31d20e001a75
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2025-12-12 22:25:11 +0530

    Time: 4 ms (82.11%), Space: 96.6 MB (25.97%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -8,21 +8,49 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public boolean isPalindrome(ListNode head) {
-        List<Integer> l = new ArrayList<>();
+// brute: iterate linkedlist, store values in List, check if list is palindrome.
+// TC - O(n), SC - O(n)
 
-        while(head!=null){
-            l.add(head.val);
-            head=head.next;
+// optimal:
+// find middle of linkedlist using slow and fast, reverse the the right half.
+// init dummy to head then compare slow and head.
+// if dummy.val = slow.val, until slow gets null, then its palindrome else its not.
+// TC : O(n)
+//SC : O(1)
+
+
+class Solution {
+
+
+    private ListNode reverseLL(ListNode head) {
+        ListNode newHead = null;
+
+        while(head != null){
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
         }
-        int left = 0;
-        int right = l.size()-1;
-        
-        while(left<right){
-            if(l.get(left)!=l.get(right)) return false;
-            left++;
-            right--;
+        return newHead;
+    }
+    public boolean isPalindrome(ListNode head) {
+        if(head==null || head.next==null) return true;
+
+        ListNode slow = head, fast=head;
+
+        while(fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast=fast.next.next;
+        }
+
+        slow.next = reverseLL(slow.next);
+
+        slow = slow.next;
+
+        while(slow!=null){
+            if(slow.val!=head.val) return false;
+            head = head.next;
+            slow=slow.next;
         }
 
         return true;
