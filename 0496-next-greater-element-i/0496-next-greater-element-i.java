3833ba99b99bcd76f245aa4e03ce04dc7b5fa575
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2025-12-15 17:03:53 +0530

    Time: 15 ms (5.9%), Space: 44.9 MB (54.13%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -2,30 +2,25 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int n = nums2.length;
         Stack<Integer> st = new Stack();
-        List<Integer> ng = new ArrayList<Integer>();
+        int[] ng = new int[n];
         
-        for(int i=0;i<n;i++){
-            int found = 0;
-            for(int j=i+1;j<n;j++){
-                if(nums2[j]>nums2[i]){
-                    ng.add(nums2[j]);
-                    found=1;
-                    break;
-                }  
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && st.peek()<=nums2[i]){
+                st.pop();
             }
-            if(found==0) ng.add(-1);
-
+            ng[i] = st.empty() ? -1 : st.peek();
+            st.push(nums2[i]);
         }
 
         int m = nums1.length;
         int[] ans = new int[m];
         List<Integer> n2List = new ArrayList<>(); 
         for (int value : nums2) {
-            n2List.add(value); 
+            n2List.add(value);
         }
         for(int i=0;i<m;i++){
             int ind = n2List.indexOf(nums1[i]);
-            ans[i] = ng.get(ind);
+            ans[i] = ng[ind];
         }
 
         return ans;
