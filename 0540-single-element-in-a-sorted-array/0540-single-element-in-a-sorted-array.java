19a92044503674ccb18fa30c156fc69c954ced61
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2025-12-23 20:36:46 +0530

    Time: 0 ms (100%), Space: 53.1 MB (5.79%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -4,8 +4,10 @@ class Solution {
         while(low<=high){
             int mid = (low+high)/2;
             if(nums[mid] == nums[mid^1]){
+                //shrink left half
                 low = mid+1;
             }
+            //shrink right half
             else high = mid-1;
         }
         return nums[low];
